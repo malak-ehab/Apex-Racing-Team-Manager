9f9cafb461b7e527fa10add8ecb0dfc3ca82d7aa
Malak <Malak_20235116@fci.capu.edu.eg>
2026-09-03 18:35:41 +0300

    Add project files.

---
+++ Apex Racing Team Manager/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Apex_Racing_Team_Manager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
